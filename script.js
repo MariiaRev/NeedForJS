8caed0714b5f3f0700897f392530a37bc78f76a8
MariiaRev <maria.revenko2209@gmail.com>
2020-09-08 14:39:18 +0300

    add another image of a player's car

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const score = document.querySelector('.score'),
 start = document.querySelector('.start'),
 gameArea = document.querySelector('.gameArea'),
 car = document.createElement('div');
+
 car.classList.add('car');
 
 
